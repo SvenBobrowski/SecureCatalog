5029dacff27dce27c5bf4a413fe4511b8796567e
Sven Bobrowski <sb@intelligentgraphics.ag>
2026-09-14 15:39:23 +0200

    Merged extended tests from auth-db branch

--- tests/SecureCatalog.Api.Tests/Factories/CustomWebApplicationFactory.cs
+++ tests/SecureCatalog.Api.Tests/Factories/CustomWebApplicationFactory.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SecureCatalog.Api.Data;
+using SecureCatalog.Api.Data.Entities;
 
 namespace SecureCatalog.Api.Tests.Factories;
 
@@ -15,9 +17,11 @@ public class CustomWebApplicationFactory
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
-        
+
         builder.ConfigureServices(services =>
         {
+            services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
             var descriptor = services.SingleOrDefault(
                 d => d.ServiceType ==
                      typeof(DbContextOptions<CatalogDbContext>));
@@ -43,6 +47,25 @@ public class CustomWebApplicationFactory
                 .GetRequiredService<CatalogDbContext>();
 
             db.Database.EnsureCreated();
+
+            var passwordHasher = scope.ServiceProvider
+                .GetRequiredService<IPasswordHasher<User>>();
+
+            var reader = new User
+            {
+                Username = "reader",
+                PasswordHash = string.Empty,
+                Role = "Reader",
+                IsActive = true
+            };
+
+            reader.PasswordHash = passwordHasher.HashPassword(
+                reader,
+                "reader123");
+
+            db.Users.Add(reader);
+
+            db.SaveChanges();
         });
     }
 
